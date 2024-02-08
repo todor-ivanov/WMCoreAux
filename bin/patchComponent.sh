3b40d18db147a329bc449f1c3d98df85026da558
Todor Ivanov <todor.trendafilov.ivanov@cern.ch>
2024-02-08 18:21:06 +0100

    Fix for missing files at the origin for patchComponent.sh

--- bin/patchComponent.sh
+++ bin/patchComponent.sh
@@ -16,21 +16,35 @@ usage()
 if [ -t 0 ] ; then pipe=false; else pipe=true ; fi
 
 # TODO: To check against a list of components
-# component=$1
-# shift
+component=$1
+shift
 patchNum=$1
 shift
 
+[[ -z $component ]] && usage
 [[ -z $patchNum ]] && patchNum=temp
 
 echo "Patching component: $component"
 
-rootDir=/usr/local/lib/python3.8/site-packages/
+if [[ $component == "wmagent" ]]
+then
+    rootDir=/data/srv/$component/current/apps.sw/$component/lib/python*/site-packages
+    rootBase=/data/srv/$component/current
+else
+    rootDir=/usr/local/lib/python3.8/site-packages/
+    rootBase=""
+fi
 
 stripLevel=3
 patchFile=/tmp/$patchNum.patch
-currTag=$(python -c "from WMCore import __version__ as WMCoreVersion; print(WMCoreVersion)")
 
+if [[ $component == "wmagent" ]]
+then
+    currTag=$(basename $(realpath $rootBase))
+    currTag=${currTag##v}
+else
+    currTag=$(python -c "from WMCore import __version__ as WMCoreVersion; print(WMCoreVersion)")
+fi
 patchCmd="patch -t --verbose -b --version-control=numbered -d $rootDir -p$stripLevel"
 
 
@@ -51,8 +65,14 @@ do
     file=${file#a\/src\/python\/}
     echo orig: https://raw.githubusercontent.com/dmwm/WMCore/$currTag/src/python/$file
     echo dest: $rootDir/$file
-    curl  https://raw.githubusercontent.com/dmwm/WMCore/$currTag/src/python/$file  -o $rootDir/$file
+    curl -f https://raw.githubusercontent.com/dmwm/WMCore/$currTag/src/python/$file  -o $rootDir/$file || { \
+        echo file: $file missing at the origin.
+        echo Seems to be a new file for the curren patch.
+        echo Removing it from the destination as well!
+        rm -f $rootDir/$file
+    }
 done
 
 echo "Patching all files starting from the original version"
+echo "cat $patchFile | $patchCmd"
 cat $patchFile | $patchCmd
