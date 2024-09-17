b20fc341ad16a6147fad5fdb0884a83f67b49d45
Todor Ivanov <todor.trendafilov.ivanov@cern.ch>
2024-09-17 09:27:52 +0200

    Add Utils from WMCore main repo

---
+++ src/python/Utils/Utilities.py
@@ -0,0 +1,315 @@
+#! /usr/bin/env python
+
+from builtins import str, bytes
+
+import subprocess
+import os
+import re
+import zlib
+import base64
+import sys
+from types import ModuleType, FunctionType
+from gc import get_referents
+from functools import reduce
+
+
+def reduceReport(reportList, expectedValue='OK'):
+    """
+    Simple function to aggregate a list of values (possibly outcomes of multiple
+    calls to couchDB or similar) to a single value.
+    :param report:        A list with accumulated report values
+    :param exepctedValue: The value with which the initial report list is expected
+                          to be filled. (any type) Default: 'OK'
+    :return:              Either the expected value (could be of any type) in the
+                          case when all the entries in the list are identical or
+                          False in the case when any of them deviates from the expected value.
+    """
+    if reduce(lambda x, y: x == y == expectedValue and expectedValue, reportList, expectedValue):
+        return expectedValue
+    return False
+
+
+def lowerCmsHeaders(headers):
+    """
+    Lower CMS headers in provided header's dict. The WMCore Authentication
+    code check only cms headers in lower case, e.g. cms-xxx-yyy.
+    """
+    lheaders = {}
+    for hkey, hval in list(headers.items()): # perform lower-case
+        # lower header keys since we check lower-case in headers
+        if hkey.startswith('Cms-') or hkey.startswith('CMS-'):
+            lheaders[hkey.lower()] = hval
+        else:
+            lheaders[hkey] = hval
+    return lheaders
+
+def makeList(stringList):
+    """
+    _makeList_
+
+    Make a python list out of a comma separated list of strings,
+    throws a ValueError if the input is not well formed.
+    If the stringList is already of type list, then return it untouched.
+    """
+    if isinstance(stringList, list):
+        return stringList
+    if isinstance(stringList, str):
+        toks = stringList.lstrip(' [').rstrip(' ]').split(',')
+        if toks == ['']:
+            return []
+        return [str(tok.strip(' \'"')) for tok in toks]
+    raise ValueError("Can't convert to list %s" % stringList)
+
+
+def makeNonEmptyList(stringList):
+    """
+    _makeNonEmptyList_
+
+    Given a string or a list of strings, return a non empty list of strings.
+    Throws an exception in case the final list is empty or input data is not
+    a string or a python list
+    """
+    finalList = makeList(stringList)
+    if not finalList:
+        raise ValueError("Input data cannot be an empty list %s" % stringList)
+    return finalList
+
+
+def strToBool(string):
+    """
+    Try to convert different variations of True or False (including a string
+    type object) to a boolean value.
+    In short:
+     * True gets mapped from: True, "True", "true", "TRUE".
+     * False gets mapped from: False, "False", "false", "FALSE"
+     * anything else will fail
+    :param string: expects a boolean or a string, but it could be anything else
+    :return: a boolean value, or raise an exception if value passed in is not supported
+    """
+    if string is False or string is True:
+        return string
+    elif string in ["True", "true", "TRUE"]:
+        return True
+    elif string in ["False", "false", "FALSE"]:
+        return False
+    raise ValueError("Can't convert to bool: %s" % string)
+
+
+def safeStr(string):
+    """
+    _safeStr_
+
+    Cast simple data (int, float, basestring) to string.
+    """
+    if not isinstance(string, (tuple, list, set, dict)):
+        return str(string)
+    raise ValueError("We're not supposed to convert %s to string." % string)
+
+
+def diskUse():
+    """
+    This returns the % use of each disk partition
+    """
+    diskPercent = []
+    df = subprocess.Popen(["df", "-klP"], stdout=subprocess.PIPE)
+    output = df.communicate()[0]
+    output = decodeBytesToUnicode(output).split("\n")
+    for x in output:
+        split = x.split()
+        if split != [] and split[0] != 'Filesystem':
+            diskPercent.append({'mounted': split[5], 'percent': split[4]})
+
+    return diskPercent
+
+
+def numberCouchProcess():
+    """
+    This returns the number of couch process
+    """
+    ps = subprocess.Popen(["ps", "-ef"], stdout=subprocess.PIPE)
+    process = ps.communicate()[0]
+    process = decodeBytesToUnicode(process).count('couchjs')
+
+    return process
+
+
+def rootUrlJoin(base, extend):
+    """
+    Adds a path element to the path within a ROOT url
+    """
+    if base:
+        match = re.match("^root://([^/]+)/(.+)", base)
+        if match:
+            host = match.group(1)
+            path = match.group(2)
+            newpath = os.path.join(path, extend)
+            newurl = "root://%s/%s" % (host, newpath)
+            return newurl
+    return None
+
+
+def zipEncodeStr(message, maxLen=5120, compressLevel=9, steps=100, truncateIndicator=" (...)"):
+    """
+    _zipEncodeStr_
+    Utility to zip a string and encode it.
+    If zipped encoded length is greater than maxLen,
+    truncate message until zip/encoded version
+    is within the limits allowed.
+    """
+    message = encodeUnicodeToBytes(message)
+    encodedStr = zlib.compress(message, compressLevel)
+    encodedStr = base64.b64encode(encodedStr)
+    if len(encodedStr) < maxLen or maxLen == -1:
+        return encodedStr
+
+    compressRate = 1. * len(encodedStr) / len(base64.b64encode(message))
+
+    # Estimate new length for message zip/encoded version
+    # to be less than maxLen.
+    # Also, append truncate indicator to message.
+    truncateIndicator = encodeUnicodeToBytes(truncateIndicator)
+    strLen = int((maxLen - len(truncateIndicator)) / compressRate)
+    message = message[:strLen] + truncateIndicator
+
+    encodedStr = zipEncodeStr(message, maxLen=-1)
+
+    # If new length is not short enough, truncate
+    # recursively by steps
+    while len(encodedStr) > maxLen:
+        message = message[:-steps - len(truncateIndicator)] + truncateIndicator
+        encodedStr = zipEncodeStr(message, maxLen=-1)
+
+    return encodedStr
+
+
+def getSize(obj):
+    """
+    _getSize_
+
+    Function to traverse an object and calculate its total size in bytes
+    :param obj: a python object
+    :return: an integer representing the total size of the object
+
+    Code extracted from Stack Overflow:
+    https://stackoverflow.com/questions/449560/how-do-i-determine-the-size-of-an-object-in-python
+    """
+    # Custom objects know their class.
+    # Function objects seem to know way too much, including modules.
+    # Exclude modules as well.
+    BLACKLIST = type, ModuleType, FunctionType
+
+    if isinstance(obj, BLACKLIST):
+        raise TypeError('getSize() does not take argument of type: '+ str(type(obj)))
+    seen_ids = set()
+    size = 0
+    objects = [obj]
+    while objects:
+        need_referents = []
+        for obj in objects:
+            if not isinstance(obj, BLACKLIST) and id(obj) not in seen_ids:
+                seen_ids.add(id(obj))
+                size += sys.getsizeof(obj)
+                need_referents.append(obj)
+        objects = get_referents(*need_referents)
+    return size
+
+
+def decodeBytesToUnicode(value, errors="strict"):
+    """
+    Accepts an input "value" of generic type.
+
+    If "value" is a string of type sequence of bytes (i.e. in py2 `str` or
+    `future.types.newbytes.newbytes`, in py3 `bytes`), then it is converted to
+    a sequence of unicode codepoints.
+
+    This function is useful for cleaning input data when using the
+    "unicode sandwich" approach, which involves converting bytes (i.e. strings
+    of type sequence of bytes) to unicode (i.e. strings of type sequence of
+    unicode codepoints, in py2 `unicode` or `future.types.newstr.newstr`,
+    in py3 `str` ) as soon as possible when recieving input data, and
+    converting unicode back to bytes as late as possible.
+    achtung!:
+    - converting unicode back to bytes is not covered by this function
+    - converting unicode back to bytes is not always necessary. when in doubt,
+      do not do it.
+    Reference: https://nedbatchelder.com/text/unipain.html
+
+    py2:
+    - "errors" can be: "strict", "ignore", "replace",
+    - ref: https://docs.python.org/2/howto/unicode.html#the-unicode-type
+    py3:
+    - "errors" can be: "strict", "ignore", "replace", "backslashreplace"
+    - ref: https://docs.python.org/3/howto/unicode.html#the-string-type
+    """
+    if isinstance(value, bytes):
+        return value.decode("utf-8", errors)
+    return value
+
+def decodeBytesToUnicodeConditional(value, errors="ignore", condition=True):
+    """
+    if *condition*, then call decodeBytesToUnicode(*value*, *errors*),
+    else return *value*
+    
+    This may be useful when we want to conditionally apply decodeBytesToUnicode,
+    maintaining brevity.
+
+    Parameters
+    ----------
+    value : any
+        passed to decodeBytesToUnicode
+    errors: str
+        passed to decodeBytesToUnicode
+    condition: boolean of object with attribute __bool__()
+        if True, then we run decodeBytesToUnicode. Usually PY2/PY3
+    """
+    if condition:
+        return decodeBytesToUnicode(value, errors)
+    return value
+
+def encodeUnicodeToBytes(value, errors="strict"):
+    """
+    Accepts an input "value" of generic type.
+
+    If "value" is a string of type sequence of unicode (i.e. in py2 `unicode` or
+    `future.types.newstr.newstr`, in py3 `str`), then it is converted to
+    a sequence of bytes.
+
+    This function is useful for encoding output data when using the
+    "unicode sandwich" approach, which involves converting unicode (i.e. strings
+    of type sequence of unicode codepoints) to bytes (i.e. strings of type
+    sequence of bytes, in py2 `str` or `future.types.newbytes.newbytes`,
+    in py3 `bytes`) as late as possible when passing a string to a third-party
+    function that only accepts bytes as input (pycurl's curl.setop is an
+    example).
+    py2:
+    - "errors" can be: "strict", "ignore", "replace", "xmlcharrefreplace"
+    - ref: https://docs.python.org/2/howto/unicode.html#the-unicode-type
+    py3:
+    - "errors" can be: "strict", "ignore", "replace", "backslashreplace", 
+      "xmlcharrefreplace", "namereplace"
+    - ref: https://docs.python.org/3/howto/unicode.html#the-string-type
+    """
+    if isinstance(value, str):
+        return value.encode("utf-8", errors)
+    return value
+
+def encodeUnicodeToBytesConditional(value, errors="ignore", condition=True):
+    """
+    if *condition*, then call encodeUnicodeToBytes(*value*, *errors*),
+    else return *value*
+    
+    This may be useful when we want to conditionally apply encodeUnicodeToBytes,
+    maintaining brevity.
+
+    Parameters
+    ----------
+    value : any
+        passed to encodeUnicodeToBytes
+    errors: str
+        passed to encodeUnicodeToBytes
+    condition: boolean of object with attribute __bool__()
+        if True, then we run encodeUnicodeToBytes. Usually PY2/PY3
+    """
+    if condition:
+        return encodeUnicodeToBytes(value, errors)
+    return value
---
+++ test/python/Utils_t/Utilities_t.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+"""
+Unittests for Utilities functions
+"""
+
+from builtins import object
+import unittest
+
+from Utils.Utilities import makeList, makeNonEmptyList, strToBool, \
+    safeStr, rootUrlJoin, zipEncodeStr, lowerCmsHeaders, getSize, \
+    encodeUnicodeToBytes, diskUse, numberCouchProcess, reduceReport
+
+
+class UtilitiesTests(unittest.TestCase):
+    """
+    unittest for Utilities functions
+    """
+
+    def testReduceReport(self):
+        """
+        Test reduceReport function
+        """
+        testList = ['OK', 'OK', 'OK']
+        self.assertEqual(reduceReport(testList), 'OK')
+        self.assertEqual(reduceReport([]), 'OK')
+        testList.append(None)
+        self.assertEqual(reduceReport(testList), False)
+
+        testList = ['nonDefStr', 'nonDefStr', 'nonDefStr']
+        self.assertEqual(reduceReport(testList, expectedValue='nonDefStr'), 'nonDefStr')
+        self.assertEqual(reduceReport([], expectedValue='nonDefStr'), 'nonDefStr')
+        testList.append(False)
+        self.assertEqual(reduceReport(testList, expectedValue='nonDefStr'), False)
+
+        testList = [True, True, True]
+        self.assertEqual(reduceReport(testList, expectedValue=True), True)
+        self.assertEqual(reduceReport([], expectedValue=True), True)
+        testList.append(False)
+        self.assertEqual(reduceReport(testList, expectedValue=True), False)
+
+        testList = [None, None, None]
+        self.assertEqual(reduceReport(testList, expectedValue=None), False)
+        self.assertEqual(reduceReport([], expectedValue=None), False)
+        testList.append(False)
+        self.assertEqual(reduceReport(testList, expectedValue=None), False)
+
+        testList = [False, False, False]
+        self.assertEqual(reduceReport(testList, expectedValue=False), False)
+        self.assertEqual(reduceReport([], expectedValue=False), False)
+        testList.append(True)
+        self.assertEqual(reduceReport(testList, expectedValue=False), False)
+
+        testList = [{'res': 'OK'}, {'res': 'OK'}, {'res': 'OK'}]
+        self.assertDictEqual(reduceReport(testList, expectedValue={'res': 'OK'}), {'res': 'OK'})
+        self.assertDictEqual(reduceReport([], expectedValue={'res': 'OK'}), {'res': 'OK'})
+        testList.append(False)
+        self.assertEqual(reduceReport(testList, expectedValue={'res': 'OK'}), False)
+
+        testList = [['OK'], ['OK'], ['OK']]
+        self.assertListEqual(reduceReport(testList, expectedValue=['OK']), ['OK'])
+        self.assertListEqual(reduceReport([], expectedValue=['OK']), ['OK'])
+        testList.append(False)
+        self.assertEqual(reduceReport(testList, expectedValue=['OK']), False)
+
+    def testMakeList(self):
+        """
+        Test the makeList function
+        """
+        self.assertEqual(makeList([]), [])
+        self.assertEqual(makeList(""), [])
+        self.assertEqual(makeList(['123']), ['123'])
+        self.assertEqual(makeList([456]), [456])
+        self.assertListEqual(makeList(['123', 456, '789']), ['123', 456, '789'])
+
+        self.assertEqual(makeList('123'), ['123'])
+        self.assertEqual(makeList('123'), ['123'])
+        self.assertListEqual(makeList('123,456'), ['123', '456'])
+        self.assertListEqual(makeList('123,456'), ['123', '456'])
+        self.assertListEqual(makeList('["aa","bb","cc"]'), ['aa', 'bb', 'cc'])
+        self.assertListEqual(makeList(' ["aa", "bb", "cc"] '), ['aa', 'bb', 'cc'])
+
+        self.assertRaises(ValueError, makeList, 123)
+        self.assertRaises(ValueError, makeList, 123.456)
+        self.assertRaises(ValueError, makeList, {1: 123})
+
+    def testLowerCmsHeaders(self):
+        "Test lowerCmsHeaders function"
+        val = 'cms-xx-yy'
+        headers = {'CAPITAL': 1, 'Camel': 1, 'Cms-Xx-Yy': val, 'CMS-XX-YY': val, 'cms-xx-yy': val}
+        lheaders = lowerCmsHeaders(headers)
+        self.assertEqual(sorted(lheaders.keys()), sorted(['CAPITAL', 'Camel', val]))
+        self.assertEqual(lheaders['CAPITAL'], 1)
+        self.assertEqual(lheaders['Camel'], 1)
+        self.assertEqual(lheaders[val], val)
+        self.assertEqual(len(lheaders), 3)
+
+    def testMakeNonEmptyList(self):
+        """
+        Test the makeNonEmptyList function.
+        It has exactly the same behaviour as makeList, but throws an exception
+        for empty list or string.
+        """
+        self.assertEqual(makeList(['123']), makeNonEmptyList(['123']))
+        self.assertListEqual(makeList(['123', 456, '789']), makeNonEmptyList(['123', 456, '789']))
+
+        self.assertListEqual(makeList('123,456'), makeNonEmptyList('123, 456'))
+        self.assertListEqual(makeList('["aa","bb","cc"]'), makeNonEmptyList('["aa", "bb", "cc"]'))
+
+        self.assertRaises(ValueError, makeNonEmptyList, [])
+        self.assertRaises(ValueError, makeNonEmptyList, "")
+
+    def testStrToBool(self):
+        """
+        Test the strToBool function.
+        """
+        for v in [True, "True", "TRUE", "true"]:
+            self.assertTrue(strToBool(v))
+        for v in [False, "False", "FALSE", "false"]:
+            self.assertFalse(strToBool(v))
+
+        for v in [1, 0, "", "alan", [], [''], {'a': 123}]:
+            self.assertRaises(ValueError, strToBool, v)
+
+    def testSafeStr(self):
+        """
+        Test the safeStr function.
+        """
+        for v in ['123', '123', 123]:
+            self.assertEqual(safeStr(v), '123')
+        self.assertEqual(safeStr(123.45), '123.45')
+        self.assertEqual(safeStr(False), 'False')
+        self.assertEqual(safeStr(None), 'None')
+        self.assertEqual(safeStr(""), "")
+
+        for v in [[1, 2], {'x': 123}, set([1])]:
+            self.assertRaises(ValueError, safeStr, v)
+
+    def testRootUrlJoin(self):
+        """
+        Test the testRootUrlJoin function.
+        """
+        urlbase = "root://site.domain//somepath"
+        self.assertEqual(rootUrlJoin(urlbase, "extend"), "root://site.domain//somepath/extend")
+        urlbase = "random"
+        self.assertEqual(rootUrlJoin(urlbase, "extend"), None)
+        urlbase = None
+        self.assertEqual(rootUrlJoin(urlbase, "extend"), None)
+
+    def testZipEncodeStr(self):
+        """
+        Test the zipEncodeStr function.
+        """
+        message = """
+%MSG-s CMSException:  AfterFile 02-Jun-2010 14:31:43 CEST PostEndRun
+cms::Exception caught in cmsRun
+---- EventProcessorFailure BEGIN
+EventProcessingStopped
+---- ScheduleExecutionFailure BEGIN
+ProcessingStopped
+---- InvalidReference BEGIN
+BadRefCore Attempt to dereference a RefCore containing an invalid
+ProductID has been detected. Please modify the calling
+code to test validity before dereferencing.
+cms::Exception going through module PatMCMatching/analyzePatMCMatching run: 1 lumi: 666672 event: 305
+---- InvalidReference END
+Exception going through path p
+---- ScheduleExecutionFailure END
+an exception occurred during current event processing
+cms::Exception caught in CMS.EventProcessor and rethrown
+---- EventProcessorFailure END
+"""
+        encodedMessage = \
+            'eNp1j8FqwzAMhu95Cl0G2yEhaXvyrU3dkkFHqfcCnq02hkQOtlz6+HM2MrbDdBLS9/1CxdNJHcsI7UnJh8GJnScBsL0yhoMbEOpV+ZqoXNVNDc1GrBuxWUMr1TucfWRJ9pKoMGMU4scHo9OtZ3C5G+O8L3OBvCPxOXiDMfpw0G5IAWEnj91b8Xvn6KbYTxPab0+ZHm0aUD7QpDn/r/qP1dFdD85e8IoBySz0Ts+j1md9y4zjxMAebGYWTsMCGE+sHeVk0JS/+Qqc79lkuNtDryN8IBLAc1VVL5+o0W8i'
+        encodedMessage = encodeUnicodeToBytes(encodedMessage)
+        self.assertEqual(zipEncodeStr(message, maxLen=300, compressLevel=9, steps=10, truncateIndicator=" (...)"),
+                         encodedMessage)
+        # Test different maximum lengths
+        # Encoded message should always be less than the maximum limit.
+        for maxLen in (800, 500, 20):
+            self.assertLessEqual(
+                    len(zipEncodeStr(message, maxLen=maxLen, compressLevel=9, steps=10, truncateIndicator=" (...)")),
+                    maxLen)
+
+    def testGetSize(self):
+        """
+        Test the getSize function.
+        """
+        for item in (1234, 1234.1234, set([12, 34]), "test", (1, 2), {'k1': 'v1'}):
+            self.assertTrue(getSize(item) > 20)
+        with self.assertRaises(TypeError):
+            getSize(zipEncodeStr)
+
+        class TestClass1():
+            def __init__(self):
+                self.data = "blah"
+
+        cls1 = TestClass1()
+        print(getSize(cls1)) # py2: 1129, py3: 205
+        self.assertTrue(getSize(cls1) > 200)
+
+        class TestClass2(object):
+            """
+            In python2, classes that inherit from `object` have a smaller
+            memory footprint
+            """
+            def __init__(self):
+                self.data = "blah"
+
+        cls2 = TestClass2()
+        print(getSize(cls2)) # py2: 426, py3: 205
+        self.assertTrue(getSize(cls2) > 200)
+
+    def testDiskUse(self):
+        """
+        Test the `diskUse` function.
+        """
+        data = diskUse()
+        # assuming nodes will always have at least 3 partitions/mount points
+        self.assertTrue(len(data) > 2)
+
+    def testNumberCouchProcess(self):
+        """
+        Test the `numberCouchProcess` function.
+        """
+        data = numberCouchProcess()
+        # there should be at least one process, but who knows...
+        self.assertTrue(data >= 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
