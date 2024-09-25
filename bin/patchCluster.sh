0fa259de6ab7852ec8d29ab815d04636b8a4ad54
Todor Ivanov <todor.trendafilov.ivanov@cern.ch>
2024-09-25 10:44:37 +0200

    Add support for patch by service

--- bin/patchCluster.sh
+++ bin/patchCluster.sh
@@ -4,16 +4,22 @@ usage()
 {
     echo -ne "A simple script to be used for patching all Running backends in"
     echo -ne "a WMCore central services K8 cluster with a patch based on an upstream PR"
-    echo -ne "Usage: \n ./patchCluster.sh [-p <"SpaceSepListOfPods">] 12077"
+    echo -ne "Usage: \n ./patchCluster.sh [-p <"SpaceSepListOfPods">] [-s <serviceName>] 12077"
     echo -ne "        -p - Space separated list of pods to be patched (Mind the quotation marks)"
+    echo -ne "        -s - Service name whose pods to be patched (if found)"
 }
 
-currPod=""
+# Set defaults
+currPods=""
+currService=""
 zeroOnly=false
-while getopts ":p:zh" opt; do
+while getopts ":p:s:zh" opt; do
     case ${opt} in
         p)
-            currPod=$OPTARG
+            currPods=$OPTARG
+            ;;
+        s)
+            currService=$OPTARG
             ;;
         z)
             zeroOnly=true
@@ -35,30 +41,52 @@ shift $(expr $OPTIND - 1 )
 
 patchNum=$1
 if $zeroOnly; then
+    echo ========================================================
     echo "Only Zeroing the code base No patches will be applied"
 elif [[ -z $patchNum ]]; then
+    echo ========================================================
     echo ERROR: No patchNum provided; exit
 else
+    echo ========================================================
     echo "Applying patch: $patchNum"
 fi
 
-currentCluster=`kubectl config get-clusters |grep -v NAME`
+currCluster=`kubectl config get-clusters |grep -v NAME`
+nameSpace=dmwm
 
 echo ========================================================
-echo CLUSTER: $currentCluster
+echo CLUSTER: $currCluster
 echo --------------------------------------------------------
 
-if [[ -n $currPod ]] ; then
-    echo WARNING: We are about to patch backend pods: $currPod at k8 cluster: $currentCluster with patchNum: $patchNum
+# First try to find any pod from the service name provided and then extend the list in currPods:
+if [[ -n $currService ]]; then
+    servicePods=`kubectl -n $nameSpace  get ep $currService -o=jsonpath='{.subsets[*].addresses[*].ip}' | tr ' ' '\n' | xargs -I % kubectl -n $nameSpace get pods  -o=name --field-selector=status.podIP=%`
+    [[ $? -eq 0 ]] || { echo "WARNING: could not find service: $currService at cluster: $currCluster"; exit ;}
+    if [[ -n $servicePods ]] ; then
+        currPods="$currPods $servicePods"
+        echo "INFO: Found the following pods for service: $currService: "
+        echo "$servicePods "
+    else
+        echo "WARNING: No pods found for service: $currService"
+        exit
+    fi
+fi
+
+if [[ -n $currPods ]] ; then
+    echo ========================================================
+    echo WARNING: We are about to patch backend pods: $currPods
+    echo WARNING: at k8 cluster: $currCluster with patchNum: $patchNum
 else
-    echo WARNING: We are about to patch any running backend at k8 cluster: $currentCluster with patchNum: $patchNum
+    echo ========================================================
+    echo WARNING: We are about to patch ANY running backend pod
+    echo WARNING: at k8 cluster: $currCluster with patchNum: $patchNum
 fi
 
 echo WARNING: Are you sure you want to continue?
 echo -n "[y/n](Default n): "
 read x && [[ $x =~ (y|yes|Yes|YES) ]] || { echo WARNING: Exit on user request!; exit 101 ;}
+echo ========================================================
 
-nameSapace=dmwm
 # podCmd="wget https://raw.githubusercontent.com/dmwm/WMCore/master/bin/patchComponent.sh -O /data/patchComponent.sh && chmod 755 /data/patchComponent.sh && sudo /data/patchComponent.sh $patchNum "
 if $zeroOnly; then
     podCmd="wget https://raw.githubusercontent.com/todor-ivanov/WMCoreAux/master/bin/patchComponent.sh -O /data/patchComponent.sh && chmod 755 /data/patchComponent.sh && sudo /data/patchComponent.sh -z $patchNum "
@@ -68,10 +96,17 @@ fi
 
 restartCmd="/data/manage restart && sleep 1 && /data/manage status"
 
-if [[ -n $currPod ]] ; then
-    runningPods=$currPod
+if [[ -n $currPods ]] ; then
+    runningPods=$currPods
 else
-    runningPods=`kubectl get pods --no-headers -o custom-columns=":metadata.name" -n $nameSapace  --field-selector=status.phase=Running`
+    # # check if any service name was actually provided at the command line and if not only then search for all running pods for the whole cluster:
+    # if [[ -n $currService ]] ; then
+    #     echo "WARNING: Requested to patch: $currService at: $currCluster but NO pods were found."
+    #     echo "WARNING: Will not patch the whole cluster. Nothing to do here, giving up now."
+    #     exit
+    # else
+    runningPods=`kubectl get pods --no-headers -o custom-columns=":metadata.name" -n $nameSpace  --field-selector=status.phase=Running`
+    # fi
 fi
 
 for pod in $runningPods
@@ -79,9 +114,9 @@ do
     echo
     echo --------------------------------------------------------
     echo $pod:
-    echo Executing: kubectl exec -it $pod -n $nameSapace -- /bin/bash -c \"$podCmd\"
-    kubectl exec -it $pod -n $nameSapace -- /bin/bash -c "$podCmd"
+    echo Executing: kubectl exec -it $pod -n $nameSpace -- /bin/bash -c \"$podCmd\"
+    kubectl exec -it $pod -n $nameSpace -- /bin/bash -c "$podCmd"
     echo
-    echo Executing: kubectl exec $pod -n $nameSapace -- /bin/bash -c \"$restartCmd\"
-    kubectl exec $pod -n $nameSapace -- /bin/bash -c "$restartCmd"
+    echo Executing: kubectl exec $pod -n $nameSpace -- /bin/bash -c \"$restartCmd\"
+    kubectl exec $pod -n $nameSpace -- /bin/bash -c "$restartCmd"
 done
